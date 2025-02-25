058a19a93d83b5935d1cdae66f6fc5027486e85d
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-02-25 23:57:13 +0200

    Ayman-commit

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,8 +1,10 @@
 import { PiCashRegisterFill } from "react-icons/pi";
+// import { RiShoppingBag3Line } from "react-icons/ri";
 export default function Logo({ className }) {
     return (
         <div className={`d-flex align-items-center gap-2 ${className}`}>
             <PiCashRegisterFill className="fs-3" style={{ color: "var(--red)" }} />
+            {/* <RiShoppingBag3Line className="fs-3" style={{ color: "var(--red)" }} /> */}
             <p className="m-0 fw-bolder" >Smart <span style={{ color: "var(--red)" }}>POS</span></p>
         </div>
     )
