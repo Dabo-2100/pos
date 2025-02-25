67aceeef7c4be9102b448a0bf16ccefeeb336ea4
Itz-Amr <khufu701@gmail.com>
2025-02-26 00:50:19 +0200

    final

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Logo from "../../components/Logo";
 import Slider from "../../components/Slider";
+import styles from "./index.module.css";
+import { ImFacebook2 } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
+import { FaEyeSlash } from "react-icons/fa";
 
 export default function LoginPage() {
   return (
@@ -19,10 +23,42 @@ export default function LoginPage() {
           and Other Transactions
         </h4>
       </div>
-      <div className="col-7 h-100 bg-warning">
-        <form className="col-5 rounded shadow bg-white">
-          <label>user name</label>
-          <input className="form-control" type="text" />
+      <div className="col-7 h-100 d-flex align-items-center justify-content-center">
+        <form className="col-6 d-flex flex-column gap-3" id={styles.login}>
+          <div className="col-12">
+            <h1>Welcome Back !</h1>
+            <p>Please, sign in to continue</p>
+          </div>
+
+          <input type="text" placeholder="Sales ID number" />
+
+          <div className="position-relative">
+            <FaEyeSlash className={styles.eyeIcon} />
+            <input type="text" placeholder="Password" />
+          </div>
+
+          <button>Sing in</button>
+
+          <span className="col-12 text-center">or</span>
+
+          <div className="d-flex justify-content-around">
+            <button className={styles.btn}>
+              <FcGoogle className={`${styles.icon} fs-3`} /> Sign up with Google
+            </button>
+
+            <button className={styles.btn}>
+              <ImFacebook2 className={`${styles.fbIcon} fs-3`} /> Sign up with
+              Facebook
+            </button>
+          </div>
+
+          <a href="" className="text-decoration-none col-12 text-center">
+            Forgot password?
+          </a>
+
+          <p className="col-12 text-center">
+            Don't have an account? Go to Register
+          </p>
         </form>
       </div>
     </div>
