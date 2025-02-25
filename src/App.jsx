5ede3ccc86d398d2ed26d3e603054e2b301ca87b
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-02-25 23:52:10 +0200

    Ayman-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login";
+import LoginPageAyman from "./pages/Ayman/LoginPageAyman";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
           <Route path="/" element={<h1>Home Page</h1>} />
           <Route path="/register" element={<h1>Register Page</h1>} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/AymanLogin" element={<LoginPageAyman/>} />
           <Route path="*" element={<h1>404 Page</h1>} />
         </Routes>
       </BrowserRouter>
